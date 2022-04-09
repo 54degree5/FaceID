e34b3187cbddef0e2cabe166b744f46a7b30313b
Alex Sukhotckii <alex.sukhotckii@gmail.com>
2022-04-09 08:31:54 +0300

    Face ID 1.0
    Face ID on my MacOS (Silicon M1):
    if I go away from my computer (camera does not see my face), it will lock, if someone (not me) is trying the right password, and i am not there, it will still not let him in!

---
+++ main.py
@@ -0,0 +1,70 @@
+import os
+import subprocess
+import cv2
+
+recognizer = cv2.face_LBPHFaceRecognizer.create()
+recognizer.read('trainer/trainer.yml')
+cascadePath = "haar.xml"
+faceCascade = cv2.CascadeClassifier(cascadePath)
+font = cv2.FONT_HERSHEY_SIMPLEX
+id = 0
+names = ['None', 'Alex Sukhotckii']
+cam = cv2.VideoCapture(0)
+cam.set(3, 640)
+cam.set(4, 480)
+minW = 0.1 * cam.get(3)
+minH = 0.1 * cam.get(4)
+tick = 0
+while True:
+    ret, img = cam.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.2,
+        minNeighbors=5,
+        minSize=(int(minW), int(minH)),
+    )
+    print("faces = " + str(len(faces)))
+    if len(faces) == 0:
+        tick += 1
+    if tick > 10:
+        os.system("""osascript -e 'tell app "System Events" to key code 12 using {control down, command down}'""")
+        tick = 0
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        id, confidence = recognizer.predict(gray[y:y + h, x:x + w])
+        if (confidence < 50):
+            tick = 0
+            id = names[id]
+            confidence = "  {0}%".format(round(100 - confidence))
+        else:
+            id = "unknown"
+            confidence = "  {0}%".format(round(100 - confidence))
+            tick += 1
+        cv2.putText(
+            img,
+            str(id),
+            (x + 5, y - 5),
+            font,
+            1,
+            (255, 255, 255),
+            2
+        )
+        cv2.putText(
+            img,
+            str(confidence),
+            (x + 5, y + h - 5),
+            font,
+            1,
+            (255, 255, 0),
+            1
+        )
+
+    cv2.imshow('camera', img)
+    k = cv2.waitKey(10) & 0xff
+    if k == 27:
+        break
+print("\n [INFO] Exiting Program and cleanup stuff")
+cam.release()
+cv2.destroyAllWindows()
